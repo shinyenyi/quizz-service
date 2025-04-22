0b7a6fdeeefd6aa30e7010490f01d299b2bc89e6
MiguelShinyenyi <miguel.shinyenyi@globallinkplus.com>
2025-04-22 21:26:34 +0300

    initialize quizz-service
    initialize spring quizz-micro-service from spring initializer

---
+++ src/main/java/com/example/quizz_service/QuizzServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizz_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzServiceApplication.class, args);
+	}
+
+}
